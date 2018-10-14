6000baf3473c6cb79c91bb35e01a06deec0f7bd3
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 20:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 }
 
 function deleteFromObjectByKey (object,key) {
-  var newObject = {Object.assign({}, object)}
-  
+  var newObject = Object.assign({}, object)
+  delete newObject.key
+  returmn
 }
 
 function destructivelyDeleteFromObjectByKey (object,key) {
-  delete.
+  delete[key]
+  return object
 }
